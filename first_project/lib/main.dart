9f8373279159d6a90e811c20cb714a581fc63882
Diogo de Paula <depaulatrabalhos@gmail.com>
2025-04-19 22:47:03 -0300

    class 3 finished

--- first_project/lib/main.dart
+++ first_project/lib/main.dart
@@ -15,52 +15,122 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Container(
-        color: Colors.white,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Container(),
+          title: Text('Tarefas'),
+          backgroundColor: Colors.amber,
+        ),
+        body: ListView(
           children: [
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Container(color: Colors.red, width: 100, height: 100),
-                Container(color: Colors.blue, width: 50, height: 50),
-              ],
-            ),
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Container(color: Colors.blue, width: 100, height: 100),
-                Container(color: Colors.red, width: 50, height: 50),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(color: Colors.green, width: 100, height: 100),
-                Container(color: Colors.yellow, width: 100, height: 100),
-                Container(color: Colors.purple, width: 100, height: 100),
-              ],
-            ),
-            Container(
-              color: Colors.amber,
-              width: 300,
-              height: 50,
-              child: const Text(
-                'Hello world!',
-                style: TextStyle(fontSize: 25, color: Colors.black),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                debugPrint('Botão clicado');
-              },
-              child: Text('Click me'),
-            ),
+            Task('Aprender Flutter'),
+            Task('Aprender Nadar'),
+            Task('Aprender Tocar Violão ao anoitecer'),
           ],
         ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.amber,
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
+
+//StatelessWidget, estático, não muda
+class Task extends StatefulWidget {
+  final String title;
+  const Task(this.title, {super.key});
+
+  @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int nivel = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsetsDirectional.all(8.0),
+      child: Stack(
+        children: [
+          Container(color: Colors.amber, height: 140),
+          Column(
+            children: [
+              Container(
+                color: Colors.blue,
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(color: Colors.black26, width: 72, height: 100),
+                    Container(
+                      width: 200,
+                      child: Text(
+                        widget.title,
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    Container(
+                      width: 55,
+                      height: 55,
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 8),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              nivel++;
+                            });
+                            // print('Nivel: $nivel'); DEBUG
+                          },
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            padding: EdgeInsets.zero,
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.arrow_drop_up, size: 30),
+                              Text('UP', style: TextStyle(fontSize: 17)),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: 200,
+                      child: LinearProgressIndicator(
+                        value: nivel / 10,
+                        color: Colors.white,
+                        backgroundColor: Colors.black38,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Text(
+                      'Nivel: $nivel',
+                      style: TextStyle(fontSize: 16, color: Colors.black),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
